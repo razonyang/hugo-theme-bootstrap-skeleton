2a23cea4fc8371b443956515edb03048ac0d3f5e
razonyang <razonyang@users.noreply.github.com>
2024-08-19 17:40:37 +0000

    deploy: e0a68707ed788c84fcc673b9be046c8f0e1ca739

---
+++ js/main-init.0b0cc4eb4a0bd2e3e1c832dd5ba71f6810d0aca704e81b7ea31d9579baadccd4.js
@@ -0,0 +1,42 @@
+(() => {
+  // ns-params:@params
+  var params_default = { baseURL: "https://projects.razonyang.com/hugo-theme-bootstrap-skeleton/" };
+
+  // ns-hugo:/tmp/hugo_cache_runner/modules/filecache/modules/pkg/mod/github.com/razonyang/hugo-theme-bootstrap@v1.6.3/assets/js/local-storage/index.ts
+  var PathLocalStorage = class {
+    constructor(baseURL) {
+      this.baseURL = baseURL;
+      if (baseURL.substring(0, 2) === "//") {
+        baseURL = "http:" + baseURL;
+      }
+      let url;
+      try {
+        url = new URL(baseURL);
+      } catch (e) {
+        url = new URL(baseURL, location.protocol + "//" + location.host);
+      }
+      const pathname = url.pathname.replace(/^(\/+)/, "").replace(/(\/+)$/, "");
+      if (pathname !== "") {
+        this.prefix += pathname.replace("/", "-") + ":";
+      }
+    }
+    prefix = "hbs:";
+    getItem(key) {
+      return localStorage.getItem(this.prefix + key);
+    }
+    setItem(key, value) {
+      localStorage.setItem(this.prefix + key, value);
+    }
+    removeItem(key) {
+      localStorage.removeItem(this.prefix + key);
+    }
+  };
+  var local_storage_default = new PathLocalStorage(params_default.baseURL);
+
+  // <stdin>
+  (() => {
+    if (local_storage_default.getItem("sidebar-toggle") !== null) {
+      document.querySelector("main")?.classList.add("sidebar-none");
+    }
+  })();
+})();
