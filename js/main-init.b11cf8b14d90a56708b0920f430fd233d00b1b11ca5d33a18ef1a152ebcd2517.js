a89680fe3c762322180e4d6639b7b9dcaf469f05
razonyang <razonyang@users.noreply.github.com>
2024-08-22 04:57:46 +0000

    deploy: 010d7490cb87514c80ed59547ad34d362c428a88

--- js/main-init.b11cf8b14d90a56708b0920f430fd233d00b1b11ca5d33a18ef1a152ebcd2517.js
+++ js/main-init.b11cf8b14d90a56708b0920f430fd233d00b1b11ca5d33a18ef1a152ebcd2517.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var params_default = { baseURL: "https://projects.razonyang.com/hugo-theme-bootstrap-skeleton/" };
 
-  // ns-hugo:/tmp/hugo_cache_runner/modules/filecache/modules/pkg/mod/github.com/razonyang/hugo-theme-bootstrap@v1.6.4/assets/js/local-storage/index.ts
+  // ns-hugo:/tmp/hugo_cache_runner/modules/filecache/modules/pkg/mod/github.com/razonyang/hugo-theme-bootstrap@v1.6.6/assets/js/local-storage/index.ts
   var PathLocalStorage = class {
     constructor(baseURL) {
       this.baseURL = baseURL;
